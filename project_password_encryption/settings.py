285c81d409496c9b54510ece58bea37e9050a1e6
Dhruv <maildhruvdas@gmail.com>
2020-07-14 20:32:08 +0530

    Updated Settings.py

--- project_password_encryption/settings.py
+++ project_password_encryption/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
 
 MEDIA_ROOT = MEDIA_DIR
-STATIC_URL = '/media/'
+MEDIA_URL = '/media/'
 
 LOGIN_URL = '/app_password_auth/user_login/'
 
